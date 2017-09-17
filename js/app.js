dc3e3786320f30b1a19c2f202468990bb857aaeb
Harish247 <g.harish247@gmail.com>
2017-09-17 02:22:02 -0700

    init commit
    * adding css,js,html files
    * loading google map
    * creating head bar
    * creating menu bar
    * creating styles
    * adding knockout functions
    * hiding and showing menu bar while clicking on the burger icon

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'),{
+        center : {lat: 37.398213, lng: -121.954562},
+        zoom: 8
+    });
+}
+
+let viewModal = {
+    showHideMenu : function(){
+        let menu = document.getElementById('menu-bar');
+        console.log(menu);
+        if(menu.style.display === 'none'){
+            menu.style.display = 'block';
+        }else{
+            menu.style.display = 'none';
+        }
+    }
+}
+ko.applyBindings(viewModal);
