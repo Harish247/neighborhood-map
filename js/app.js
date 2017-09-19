ed4c3bb7beed53b36a931abd426279ebb36d7ae4
Harish247 <g.harish247@gmail.com>
2017-09-19 00:09:16 -0700

    loading data
    * created a file with data
    * loaded data to html
    * added search function
    * added styles

--- js/app.js
+++ js/app.js
@@ -5,14 +5,28 @@ function initMap(){
     });
 }
 
-let viewModal = {
-    showHideMenu : function(){
+function viewModal(){
+    let self = this;
+    self.searchString = ko.observable('');
+    self.showHideMenu = function(){
         let menu = document.getElementById('menu-bar');
         if(menu.style.display === 'none'){
             menu.style.display = 'block';
         }else{
             menu.style.display = 'none';
         }
-    }
+    };
+    self.filteredStations = ko.computed(function(){
+        let fStations=[];
+        let sString = self.searchString().trim().toLowerCase();
+        stations.forEach(function(element) {
+            if(sString.length == 0){
+             fStations.push(element.name);   
+            }else if(element.name.toLowerCase().includes(sString)){
+                fStations.push(element.name);
+            }
+        });
+        return fStations;
+    });
 }
-ko.applyBindings(viewModal);
+ko.applyBindings(viewModal());
